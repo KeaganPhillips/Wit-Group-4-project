c6e117db1b65169399772940a586a645d5d19be9
keagan Phillips <keaganp@gmail.com>
2012-04-28 11:10:03 +0200

    adding core projects

---
+++ UnitTestReflector.Core/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestReflector.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ UnitTestVisualiser/Content/ClassDiagram.coffee
@@ -0,0 +1,2 @@
+﻿$ ->
+  class ClassDiagram extends Backbone.View
