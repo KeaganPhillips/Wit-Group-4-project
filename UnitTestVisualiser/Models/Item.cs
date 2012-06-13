9aaedc77621b5022f50172d70b1069baf2424520
Boreli Kuleile <boreli.kuleile@gmail.com>
2012-06-13 20:25:47 +0200

    Done changes on the pdf viewer

---
+++ UnitTestVisualiser/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UnitTestVisualiser.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Place { get; set; }
+    
+    }
+}
