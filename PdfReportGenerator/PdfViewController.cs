8a6af98379f328d8715b5de3acc7d696ffaec496
Boreli Kuleile <boreli.kuleile@gmail.com>
2012-06-19 11:05:51 +0200

    Fixed the pdf generator project

---
+++ PdfReportGenerator/PdfViewController.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace ReportManagement
+{
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// Extends the controller with functionality for rendering PDF views
+    /// </summary>
+    public class PdfViewController : Controller
+    {
+        private readonly HtmlViewRenderer htmlViewRenderer;
+        private readonly StandardPdfRenderer standardPdfRenderer;
+
+        public PdfViewController()
+        {
+            this.htmlViewRenderer = new HtmlViewRenderer();
+            this.standardPdfRenderer = new StandardPdfRenderer();
+        }
+
+        protected ActionResult ViewPdf(string pageTitle, string viewName, object model)
+        {
+            // Render the view html to a string.
+            string htmlText = this.htmlViewRenderer.RenderViewToString(this, viewName, model);
+
+            // Let the html be rendered into a PDF document through iTextSharp.
+            byte[] buffer = standardPdfRenderer.Render(htmlText, pageTitle);
+
+            // Return the PDF as a binary stream to the client.
+            return new BinaryContentResult(buffer, "application/pdf");
+        }
+    }
+}
